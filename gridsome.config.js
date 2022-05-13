59abd05287318ea63de2ae0e5c041b49988d712e
Travis Frazier <Travis.Frazier@ivanti.com>
2022-05-13 08:19:50 -0700

    updates

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,26 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/sass/_index.scss'),
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [],
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  }
 }
